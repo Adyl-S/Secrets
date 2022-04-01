190000806dc3ba655100fa6f06d906ea315ceb8b
Adil <zaroonraza08@gmail.com>
2022-04-01 19:17:04 +0530

    added procfile

--- app.js
+++ app.js
@@ -174,6 +174,6 @@ app.post("/login", function (req, res) {
   });
 });
 
-app.listen(3000, function () {
+app.listen(process.env.PORT || 3000, function () {
   console.log("app started at port 3000");
 });
